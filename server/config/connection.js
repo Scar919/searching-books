b48d1666eed710b59911e11008c163206d0137bc
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-05-31 16:38:04 -0700

    install apollo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+useNewUrlParser: true,
+UueUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false //check if false or true w/ deployment 
+});
+
 
 module.exports = mongoose.connection;
