ffb7c6302c51c3877ff4633b010c70f99b72bc8d
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-05-31 17:51:10 -0700

    added schema

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,74 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User} = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+        .select('-__v -password')
+        return userData;
+      }
+      throw new AuthenticationError('log in!');
+    },
+  },
+
+  Mutation: {
+  
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!profile) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, profile };
+    },
+
+    addUser: async (parent, args) => {
+      const user =await User.create(args);
+      const token =signToken(user);
+
+
+      return {user, token};
+    },
+    saveBook: async (parent, { input }, { user } ) => {
+        if (user) {
+            const updatedUser = await User.findByIdAndUpdate(
+                {_id: user._id},
+                { $addToSet: { savedBooks: input } },
+                { 
+                    new: true,
+                    runValidators: true,
+                }
+            )
+            return updatedUser;
+      }
+      throw new AuthenticationError('log in!');
+    },
+    removeBook: async (parent, { bookId }, { user } ) => {
+        if (user) {
+            const updatedUser = await User.findOneAndUpdate(
+                {_id: user._id},
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { 
+                    new: true,
+                    runValidators: true,
+                }
+            )
+            return updatedUser;
+        }
+        throw new AuthenticationError('log in!');
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+       me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook: (input: bookInput): User
+    removeBook(bookId: ID!): User
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    images: String
+  }
+
+  input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    images: String
+  }
+`;
+
+module.exports = typeDefs;
